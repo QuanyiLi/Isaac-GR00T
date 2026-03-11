40c56754cd014c82e1c7a1689819b4c1da48b49f
QuanyiLi <quanyili0057@gmail.com>
2026-03-11 09:35:35 +0000

    restore

--- modality_config.py
+++ modality_config.py
@@ -26,7 +26,7 @@ modality_config = {
         ],
     ),
     "action": ModalityConfig(
-        delta_indices=list(range(0, 20)),
+        delta_indices=list(range(0, 16)),
         modality_keys=[
             "joint_action",
             "gripper_action",
